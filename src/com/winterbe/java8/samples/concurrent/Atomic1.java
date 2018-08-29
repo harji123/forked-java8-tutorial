6ff9812af1b25306afa948173772fabc8b0db1a1
Harji <harji.virk@gmail.com>
2018-08-29 13:54:13 -0400

    hi

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 /**
- * @author Benjamin Winterberg
+ * @author Benjamin Winterberghtrhrthrghtr
  */
 public class Atomic1 {
 
